44787791409657d67b04ab6f0ea213daf4515a61
klemanroy <krsenyahan.work@gmail.com>
2022-05-11 18:56:38 +0800

    model plan and approaches

---
+++ to-do.R
@@ -0,0 +1,28 @@
+dealing with large dataset or redundant variables
+  - lasso regression
+  - data reduction such as PCA
+  - exploration:
+      correlation matrix: https://towardsdatascience.com/how-to-create-a-correlation-matrix-with-too-many-variables-309cc0c0a57
+
+ways to deal with imbalanced dataset
+  - replace evaluation metrics such as precision, recall, and F1-score
+     - precision: goal is to have a high precision value
+     - recall: goal is to have a high recall value
+     - f1-score:goal is to have a high f1-score value
+    ROC curve for precision and recall, cohens kappa, confusion matrix
+  - undersampling
+      - random
+      - informative
+          - EasyEnsemble
+          - BalanceCascade
+  - oversampling
+      - random
+      - informative
+  - SMOTE
+  - cost sensitive learning (CSL)
+
+methods t try first 
+  - decision tree
+  - randome forest
+  - logistic regression
+  - neural networks
